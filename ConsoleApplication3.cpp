82ab778e7eb498c7fbe9d2609ed9c5f98372e6f5
chertousovapolina <115408269+chertousovapolina@users.noreply.github.com>
2022-12-01 20:16:21 +0300

    Add files via upload

---
+++ ConsoleApplication3.cpp
@@ -0,0 +1,25 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+struct gnom{
+char sex;
+int age;
+int height;
+};
+int main() {
+
+	gnom gnomik;
+	gnomik.sex = 'male';
+	gnomik.age = 231;
+	gnomik.height = 13;
+
+	gnom liliput;
+	liliput.sex = 'fem';
+	liliput.age = 0;
+	liliput.height = 123;
+
+   
+
+}
